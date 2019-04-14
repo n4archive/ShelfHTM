d30cd47c103f232edd71cf82d6d0cff572bee354
nift4 <36458596+nift4@users.noreply.github.com>
2019-04-14 10:48:39 +0200

    Update

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 USG="Usage:\n$0 <htmsrc> <htmbin> <assets>"
-echo "ShelfHTM/1.0 ShelfComp-SH/1.0"
-echo
-echo "This application needs 'file', 'find' and 'python3'"
+echo "ShelfHTM/1.0 Shelf-CompSH/1.0"
 echo
 echo
 [ x$1 == x ] && echo -e $USG && exit 1
@@ -14,7 +12,7 @@ if [ ! -d $3 ];then echo -e $USG;exit 1;fi
 echo "Compiling..."
 mkdir .sbs
 echo "RklMRT0iJDEiCkZSST0ic2hlbGY6JEZJTEUiCkZNSU1FPWBmaWxlIC1iIC0tbWltZS10eXBlICRGSUxFfHNlZCAtZSAicy9cXC8vXFw/L2ciYApiYXNlNjQgLXcgMCAkRklMRT4uLi8uc2JzL3RtcApGUlA9ImRhdGE6JEZNSU1FO2Jhc2U2NCxfaV9kYXRfIgpUTVA9YHNlZCAtZSAicy8kRlJJLyRGUlAvZyIgPCQzYAplY2hvICRUTVB8c2VkIC1lICJzL1xcPy9cXC8vZyIgPiQzCnB5dGhvbjMgLWMgImY9b3BlbihcIi4uLy5zYnMvdG1wXCIsXCJyXCIpO2YyPW9wZW4oXCIkM1wiLFwiclwiKTt4PWYyLnJlYWQoKS5yZXBsYWNlKFwiX2lfZGF0X1wiLGYucmVhZCgpKTtmLmNsb3NlKCk7ZjIuY2xvc2UoKTtmMz1vcGVuKFwiJDNcIixcIndcIik7ZjMud3JpdGUoeCk7ZjMuY2xvc2UoKTsiCg==" | base64 -d > .sbs/buildhelper.sh
-echo "WD1gZWNobyAkMSB8IGN1dCAtYyA0LWAKY3AgJDIvJFggJDMvJFgKY2QgJDQKZmluZCAqIC10eXBlIGYgLWV4ZWMgYmFzaCAuLi8uc2JzL2J1aWxkaGVscGVyLnNoIHt9IC4uLyQyLyRYIC4uLyQzLyRYICI7IgpjZCAuLgo=" | base64 -d > .sbs/file.sh
+echo "TEVOPWBlY2hvICR7IzJ9ICsgMSB8IGJjYApYPWBlY2hvICQxIHwgY3V0IC1jICR7TEVOfS1gCmNwICQyLyRYICQzLyRYCmNkICQ0CmZpbmQgKiAtdHlwZSBmIC1leGVjIGJhc2ggLi4vLnNicy9idWlsZGhlbHBlci5zaCB7fSAuLi8kMi8kWCAuLi8kMy8kWCAiOyIKY2QgLi4K" | base64 -d > .sbs/file.sh
 find $1 -type f -exec bash .sbs/file.sh {} $1 $2 $3 ";"
 rm -rdf .sbs
 echo "Finished."
